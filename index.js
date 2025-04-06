affa705c31984f9441dce9335c600346d13f130d
wirowanie800 <164885772+wirowanie800@users.noreply.github.com>
2025-04-06 22:19:33 +0200

    Remove sidebar from JS

--- index.js
+++ index.js
@@ -25,18 +25,3 @@ prefersDarkMode.addEventListener('change', (e) => {
     setTheme(e.matches);
   }
 });
-
-function openTab(evt, tabId) {
-  const contents = document.querySelectorAll(".tab-content");
-  const buttons = document.querySelectorAll(".tab-button");
-
-  contents.forEach(c => c.classList.remove("active"));
-  buttons.forEach(b => b.classList.remove("active"));
-
-  document.getElementById(tabId).classList.add("active");
-  evt.currentTarget.classList.add("active");
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  initTheme();
-});
